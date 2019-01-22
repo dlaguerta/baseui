b0150bd09c5f03a00a78795fc86344445ba4cf46
Nadiia Dmytrenko <nadiia@uber.com>
2019-01-22 11:22:55 -0800

    docs(datepicker): move datepicker to docs site

--- documentation-site/routes.js
+++ documentation-site/routes.js
@@ -78,6 +78,10 @@ const routes = [
             text: 'Textarea',
             path: getPath('/components/textarea'),
           },
+          {
+            text: 'Datepicker',
+            path: getPath('/components/datepicker'),
+          },
         ],
       },
       {
